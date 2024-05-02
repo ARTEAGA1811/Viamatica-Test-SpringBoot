2a1c4854b231fc981cdc2d70484766068db36af7
xArt3x - David Arteaga <152233315+xArt3x@users.noreply.github.com>
2024-05-02 08:54:21 -0500

    initial commit

---
+++ src/test/java/com/viamatica/viamatica/ViamaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viamatica.viamatica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViamaticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
